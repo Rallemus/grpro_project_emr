86b9863385707d2a105baf4aef86042228e50641
Elise Jarnved <elisejarnved@gmail.com>
2014-11-27 14:55:13 +0100

    Added class Theater

---
+++ src/Theater.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Elise on 27-11-2014.
+ */
+public class Theater {
+    private int  seats;
+    private int rows;
+    private int total_seats;
+    private int available_seats;
+    private int reserved_seats;
+
+
+    /**
+     * The constructor
+     */
+    public Theater() {
+
+    }
+
+    /**
+     * @return returns the total number of seats in the given theater.
+     */
+    public int getTotal_seats() {
+        return total_seats;
+    }
+}
