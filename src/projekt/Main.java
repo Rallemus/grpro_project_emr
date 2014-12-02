b6e841942ba5f0a8b8450ab55c4cd53c90aa1105
Rasmus Bruun <r.b.jorgensen@gmail.com>
2014-12-02 10:44:35 +0100

    Der er lavet et testprojekt i JavaFX, magen til dét fra instruktionsvideoen. Vi kan bruge det som basis, til at bygge videre på.

---
+++ src/projekt/Main.java
@@ -0,0 +1,55 @@
+package projekt;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import projekt.controller.TodoController;
+import projekt.model.Todo;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+/**
+ * Created by Rasmus on 01-12-2014.
+ */
+public class Main extends Application {
+
+    private ObservableList<Todo> todoData = FXCollections.observableArrayList();
+
+
+    public Main() {
+        todoData.add(new Todo("program javafx APP", "programmering", LocalDate.of(2014, 10, 22)));
+        todoData.add(new Todo("clean the bathroom", "programmering", LocalDate.of(2014,10,29)));
+        todoData.add(new Todo("hand in project", "programmering", LocalDate.of(2014,11,1)));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        showTodoView(primaryStage);
+    }
+
+    public void showTodoView(Stage stage) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("view/todo.fxml"));
+            Parent toDoView = loader.load();
+            stage.setTitle("Todo");
+            stage.setScene(new Scene(toDoView, 300, 275));
+
+            TodoController controller = loader.getController();
+            controller.setData(todoData);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/projekt/model/Todo.java
@@ -0,0 +1,64 @@
+package projekt.model;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by Rasmus on 01-12-2014.
+ */
+public class Todo {
+    private StringProperty task = new SimpleStringProperty();
+    private StringProperty project = new SimpleStringProperty();
+    private ObjectProperty<LocalDate> dueDate = new SimpleObjectProperty<LocalDate>();
+    public static final DateTimeFormatter DueDateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public Todo(String task, String project, LocalDate dueDate) {
+        this.task.set(task);
+        this.project.set(project);
+        this.dueDate.set(dueDate);
+    }
+
+    public String getTask() {
+        return task.get();
+    }
+
+    public void setTask(String task) {
+        this.task.set(task);
+    }
+
+    public StringProperty taskProperty() {
+        return task;
+    }
+
+
+    public String getProject() {
+        return project.get();
+    }
+
+    public void setProject(String project) {
+        this.project.set(project);
+    }
+
+    public StringProperty projectProperty() {
+        return project;
+    }
+
+
+    public LocalDate getDueDate() {
+        return dueDate.get();
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate.set(dueDate);
+    }
+
+    public ObjectProperty<LocalDate> dueDateProperty() {
+        return dueDate;
+    }
+
+}
