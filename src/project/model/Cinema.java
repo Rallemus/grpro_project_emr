f06e82b8faa569d71f8cb97a38a06780d431de60
Maria Weybøl <mwey@itu.dk>
2014-12-03 16:21:37 +0100

    change name from mainscreen to cinema

--- src/project/model/Cinema.java
+++ src/project/model/Cinema.java
@@ -2,38 +2,42 @@ package project.model;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ChoiceBox;
 import project.DatabaseLoad;
 
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
  * Created by Maria on 02-12-2014.
  */
 public class Cinema {
-    private String shows;
+    private String shows1;
     private String date;
     private String time;
     private DatabaseLoad database = new DatabaseLoad();
 
 
-    public Cinema(String shows, String date, String time){
-        this.shows = time;
-        this.date = time;
+    public Cinema(String shows1, String date, String time){
+        this.shows1 = shows1;
+        this.date = date;
         this.time = time;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Cinema cinema = new Cinema(shows, date, time);
         cinema.getShow();
         cinema.getDate();
         cinema.getTime();
     }
 
-    public ObservableList<String> getShow()  {
-        ObservableList<String> data = new FXCollections.observableArrayList();
-        data.addAll(database.getFromDatabase(ShowTitle, shows));
+    public void getShow() throws UnsupportedEncodingException {
+        ObservableList<String> showlist = FXCollections.observableArrayList();
+        final ChoiceBox<String> chooser = new ChoiceBox<>(showlist);
+        chooser.getSelectionModel().select(0);
     }
+
 
     public ObservableList<String> getDate()  {
         ObservableList<String> data = new FXCollections.observableArrayList();
